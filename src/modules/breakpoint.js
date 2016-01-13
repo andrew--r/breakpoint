27c7ae04e37d8f4b8f68c64613e024d371f0ed0c
andrew-r <andrew-r>
2016-01-13 08:55:36 +0600

    Small updates

--- src/modules/breakpoint.js
+++ src/modules/breakpoint.js
@@ -1,8 +1,8 @@
 export default function({query, success, fail, context = null}) {
 	let mql = window.matchMedia(query);
 	let handler = function() {
-		this.matches ? success.call(context) : (fail && fail.call(context));
-	}.bind(mql);
+		mql.matches ? success.call(context) : (fail && fail.call(context));
+	};
 
 	mql.addListener(handler);
 	document.addEventListener('DOMContentLoaded', handler);
